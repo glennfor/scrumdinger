7cbf04e8d677688c6f0dd3bf4cc7024cde23f637
glennfor <glennfor72@gmail.com>
2024-06-14 04:22:04 -0400

    feat: setup scrumdinger app

---
+++ scrumdinger/MeetingView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  scrumdinger
+//
+//  Created by Glen Nfor on 6/14/24.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Seconds Elasped")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    MeetingView()
+}
