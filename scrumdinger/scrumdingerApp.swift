65370dfd80ea097f23a88e6280e1b03488b13265
glennfor <glennfor72@gmail.com>
2024-06-14 03:44:58 -0400

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Glen Nfor on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
