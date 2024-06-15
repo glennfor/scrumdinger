1dfe0ae12b65cfba2fecff4ada02b86ae4e36d07
glennfor <glennfor72@gmail.com>
2024-06-15 15:38:35 -0400

    feat: complete meeting view

--- scrumdinger/MeetingView.swift
+++ scrumdinger/MeetingView.swift
@@ -24,8 +24,20 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityValue("10 minutes")
+            Circle().strokeBorder(lineWidth: 24)
+            HStack{
+                Text("Speaker 1 of 3")
+                Button(action: {
+                    
+                }){
+                Image(systemName: "forward.fill")
+                }.accessibilityLabel("Next speaker")
+            }
         }
-        
+        .padding()
     }
 }
 
